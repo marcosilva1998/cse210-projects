f991ac997ac7e3b3c5abf84515196923cf0fc1b5
Marco <marksilvamoreno@hotmail.com>
2023-05-04 01:01:10 -0300

    Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company="Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear=0;
+        job1._endYear=3;
+
+        Job job2 = new Job();
+        job2._company="Apple";
+        job2._jobTitle = "DevOps";
+        job2._startYear=4;
+        job2._endYear=6;
+
+
+        Resume myResume = new Resume();
+        myResume._name="Marco Silva";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+        
     }
 }
