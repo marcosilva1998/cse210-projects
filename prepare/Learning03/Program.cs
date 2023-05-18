7d92cfa284c806467b095f48cde3b0de6137d5a5
Marco <marksilvamoreno@hotmail.com>
2023-05-18 18:22:30 -0300

    W05 Prepare: Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction try1 =new Fraction(1);
+        Fraction try2 =new Fraction(5);
+        Fraction try3 =new Fraction(3,4);
+        Fraction try4 =new Fraction(1,3);
+
+        Console.WriteLine( try1.GetFractionString());
+        Console.WriteLine( try1.GetDecimalValue());
+        
+        Console.WriteLine( try2.GetFractionString());
+        Console.WriteLine( try2.GetDecimalValue());
+
+        Console.WriteLine( try3.GetFractionString());
+        Console.WriteLine( try3.GetDecimalValue());
+
+        Console.WriteLine( try4.GetFractionString());
+        Console.WriteLine( try4.GetDecimalValue());
+
+
+
+        
+        
+      
+
     }
 }
