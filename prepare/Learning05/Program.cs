6d27f3cb4fa70738e71bf47f7327fd5b5e68696e
Marco <marksilvamoreno@hotmail.com>
2023-06-16 19:26:22 -0300

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List <Shape> shapes = new List<Shape>();
+        Square square = new Square("red",5);
+        Rectangle rectangle = new Rectangle("red",5,6);
+        Circle circle = new Circle("red",5);
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+        
+        for(int i=0;i<shapes.Count;i++){
+            Console.WriteLine(shapes[i].GetArea());
+            Console.WriteLine(shapes[i].GetColor());
+        }
+        Console.Read();
     }
 }
---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,15 @@
+public class Square:Shape{
+    private double _side;
+
+
+
+    public Square(string color,double side):base(color){
+        this._side=side;
+    }
+    public override double GetArea(){
+        return this._side*this._side;
+
+    }
+
+}
+
